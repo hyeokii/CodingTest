ecf222da4fc20c6c9146fb653d50c01ccfbb56df
KimJaeHyeok <92020565+hyeokii@users.noreply.github.com>
2023-11-18 23:19:48 +0900

    [level unrated] Title: 공 던지기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/unrated/120843. 공 던지기/공 던지기.js
@@ -1,10 +1,3 @@
 function solution(numbers, k) {
-    let ball = 1;
-    for(let i=0;i<k-1;i++){
-        ball += 2;
-        if(ball > numbers.length){
-            ball -= numbers.length;
-        }
-    }
-    return ball;
+    return numbers[(2*(k-1))%numbers.length];
 }
